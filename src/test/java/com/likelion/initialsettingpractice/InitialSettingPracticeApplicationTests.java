5c9e993b85f14ae85710f66562ea155dc94c2e5b
shinchaerin79 <shinchaerin79@gmail.com>
2025-07-18 14:53:12 +0900

    :tada: Start: Start New Project

---
+++ src/test/java/com/likelion/initialsettingpractice/InitialSettingPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.initialsettingpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitialSettingPracticeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
